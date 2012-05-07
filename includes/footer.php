eceedf457807cf0c0a7e699f257278d79540a3be
Nathaniel Higgins <nat@nath.is>
2012-05-07 12:16:04 +0100

    Add Analytics Tracking Code

--- includes/footer.php
+++ includes/footer.php
@@ -9,5 +9,19 @@
 			<img src="<?php echo theme_url('img/anchor.png'); ?>" alt="Anchor" class="anchor">
 		</a>
 	</footer>
-</div></body>
+</div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22931342-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
 </html>
