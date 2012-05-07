49dd19f4f480fc543ff6be7c901ed7146de7e079
Nathaniel Higgins <nat@nath.is>
2012-05-07 02:36:46 +0100

    Add contribution to anchor

--- includes/footer.php
+++ includes/footer.php
@@ -5,6 +5,9 @@
 			Contact me on <a href="<?php echo twitter_url(); ?>">Twitter</a>, or check out some of my latest work on
 			<a href="http://forrst.nath.is">Forrst</a> or <a href="http://github.com/nathggns">Github</a>
 		</div>
+		<a href="http://anchorcms.com">
+			<img src="<?php echo theme_url('img/anchor.png'); ?>" alt="Anchor" class="anchor">
+		</a>
 	</footer>
 </div></body>
 </html>
