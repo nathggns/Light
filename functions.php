dc2e9c7e7cdb1659dcf88f12757a7828144cbd2c
Nathaniel Higgins <nat@nath.is>
2012-05-13 18:31:44 +0100

    Add network users to local test

--- functions.php
+++ functions.php
@@ -6,7 +6,9 @@ function theme_folder($append = '') {
 	return getcwd() . str_replace(base_url(), '/', theme_url()) . $append;
 }
 function is_local() {
-	return $_SERVER['REMOTE_ADDR'] === "127.0.0.1" || $_SERVER['REMOTE_ADDR'] === "81.101.46.47";
+	return $_SERVER['REMOTE_ADDR'] === "127.0.0.1" ||
+		$_SERVER['REMOTE_ADDR'] === "81.101.46.47" ||
+		strpos($_SERVER['REMOTE_ADDR'], '172') !== false;
 }
 function is_single() {
 	return !is_postspage();
