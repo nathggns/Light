50f6d0ec590f777fb84a54704c1acc18e31a8a97
Nathaniel Higgins <nat@nath.is>
2012-05-15 01:17:30 +0100

    Add mark to page

--- includes/footer.php
+++ includes/footer.php
@@ -9,35 +9,38 @@
 		<a href="http://anchorcms.com">
 			<img src="<?php echo theme_url('img/anchor.png'); ?>" alt="Anchor" class="anchor">
 		</a>
+		<a href="http://nath.is">
+			<img src="<?php echo theme_url('img/mark.png'); ?>" alt="" class="mark">
+		</a>
 	</footer>
 </div>
 <?php if (!is_local()): ?>
 <script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-22931342-2']);
-  _gaq.push(['_trackPageview']);
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-22931342-2']);
+	_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 
 </script>
 <script type="text/javascript">
-    var GoSquared={};
-    GoSquared.acct = "GSN-775395-P";
-    (function(w){
-        function gs(){
-            w._gstc_lt=+(new Date); var d=document;
-            var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
-            var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);
-        }
-        w.addEventListener?w.addEventListener("load",gs,false):w.attachEvent("onload",gs);
-    })(window);
+		var GoSquared={};
+		GoSquared.acct = "GSN-775395-P";
+		(function(w){
+				function gs(){
+						w._gstc_lt=+(new Date); var d=document;
+						var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+						var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);
+				}
+				w.addEventListener?w.addEventListener("load",gs,false):w.attachEvent("onload",gs);
+		})(window);
 </script>
-          
+					
 <?php endif; ?>
 </body>
 </html>
