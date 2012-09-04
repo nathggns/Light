195704f690e7248b497a93d996cbd07021f33efe
Nathaniel Higgins <nat@nath.is>
2012-09-04 13:16:43 +0100

    Add author picture to Google

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,7 @@ $title .= site_name();
 	<link rel="stylesheet" href="<?php echo theme_url('css/main.css'); ?>" type="text/css">
 	<link rel="shortcut icon" href="<?php echo theme_url('favicon.png'); ?>">
 	<link rel="alternate" type="application/rss+xml" title="Feed | <?php echo site_name(); ?>" href="<?php echo rss_url(); ?>" />
+	<link rel="author" href="https://plus.google.com/107924709939978010057/posts">
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" src="http://assets.nath.is/js/cprompt.min.js"></script>
 	<script type="text/javascript" src="<?php echo theme_url('js/lib.js'); ?>"></script>
