b528e87c31b69526ddb00b1e07d0d9ca54ece3ab
Nathaniel Higgins <nat@nath.is>
2012-09-28 21:41:04 +0100

    Remove cprompt

--- includes/footer.php
+++ includes/footer.php
@@ -24,7 +24,6 @@
 	_gaq.push(['_trackPageview']);
 
 	(function() {
-		if (!cPrompt.allowCookies()) return false;
 		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
@@ -35,7 +34,6 @@
 		var GoSquared={};
 		GoSquared.acct = "GSN-775395-P";
 		(function(w){
-			if (!cPrompt.allowCookies()) return false;
 			function gs(){
 					w._gstc_lt=+(new Date); var d=document;
 					var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
